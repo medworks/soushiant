914f89f2cf0f4cafef0bc14bdec842ce39602658
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-28 19:49:27 +0330

    change folder

---
+++ themes/inc/contact.php
@@ -0,0 +1,25 @@
+<div class="pages rtl" id="contact">
+	<div class="contact">
+	    <h1>تماس با ما</h1>
+	    <p>شما می توانید از طریق ارسال فرم زیر با ما در تماس باشید</p>
+	    <form class="form" name="contact">
+	        <p class="error_message"><strong>ERROR:</strong> please fill the required fields (Full Name, Email and Message).</p>
+	        <p>
+	            <label for="full_name">نام و نام خانوادگی</label>
+	            <input type="text" name="full_name" class="required" id="full_name">
+	        </p>
+	        <p>
+	            <label for="email">ایمیل</label>
+	            <input type="text" name="email" class="required" id="email">
+	        </p>
+	        <p>
+	            <label for="message">پیام</label>
+	            <textarea name="message" class="required" id="message"></textarea>
+	        </p>
+	        <p>
+	            <input type="submit" class="btn" value="ارسال">
+	            <span class="confirm">متشکر. پیام شما ارسال شد. به زودیی پاسخ داده داده شد</span>
+	        </p>
+	    </form>
+	</div>
+</div>
