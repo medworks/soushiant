080ab1f48a5b1c741e33639e2151ce4c16545d25
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-10-21 13:08:39 +0330

    add table for price

--- themes/table.php
+++ themes/table.php
@@ -1,8 +1,39 @@
 <?php
-$html="<table><tbody>";
+$html="<table border='0' cellpadding='0' cellspacing='0'><tbody>";
 
 $html.=<<<cd
-	<tr></tr>
+		<tr>
+			<td class="tdtitle" style="border-right: 0px;" valign="baseline">نام سرویس: ECO128</td>
+			<td colspan="4" class="tdtitle" style="border-left: 0px;" valign="baseline">سرعت تا:&nbsp; 128/<strong>128</strong>کیلو بیت بر ثانیه</td>
+		</tr>
+		<tr>
+			<td class="tdbox" valign="baseline" width="*">مدت سرویس</td>
+			<td class="tdbox align-c" valign="baseline" width="100">یک ماهه</td>
+			<td class="tdbox align-c" valign="baseline" width="100">سه ماهه</td>
+			<td class="tdbox align-c" valign="baseline" width="100">شش ماهه</td>
+			<td class="tdbox lcol align-c" valign="baseline" width="100">یک ساله</td>
+		</tr>
+			<tr>
+			<td class="tdbox" valign="baseline">میزان ترافیک/ گیگا بایت</td>
+			<td class="tdbox align-c" valign="baseline">۳</td>
+			<td class="tdbox align-c" valign="baseline">۶</td>
+			<td class="tdbox align-c" valign="baseline">۱۰</td>
+			<td class="tdbox lcol align-c" valign="baseline">۱۵</td>
+		</tr>
+		<tr>
+			<td class="tdbox" valign="baseline">هزینه اشتراک / تومان</td>
+			<td class="tdbox align-c" valign="baseline">۱۳٫۹۰۰</td>
+			<td class="tdbox align-c" valign="baseline">۲۹٫۹۰۰</td>
+			<td class="tdbox align-c" valign="baseline">۵۲٫۹۰۰</td>
+			<td class="tdbox lcol align-c" valign="baseline">۸۷٫۹۰۰</td>
+		</tr>
+		<tr>
+			<td class="tdbox even" valign="baseline">متوسط هزینه برای هر ماه / تومان</td>
+			<td class="tdbox even align-c" valign="baseline">۱۳٫۹۰۰</td>
+			<td class="tdbox even align-c" valign="baseline">۹٫۹۰۰</td>
+			<td class="tdbox even align-c" valign="baseline">۸٫۸۰۰</td>
+			<td class="tdbox lcol even align-c" valign="baseline">۷٫۳۰۰</td>
+		</tr>
 cd;
 
 $html.=<<<cd
