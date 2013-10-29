8f95e4fcd3b484cea9b15d94d7b221fa17c5207a
Saeid Hatami <hatami4510@gmail.com>
2013-10-29 18:53:34 +0330

    add php code for getting dynamic data

---
+++ themes/inc/internet.php
@@ -0,0 +1,31 @@
+<?php
+	include_once("../../config.php");
+    include_once("../../classes/database.php");
+	include_once("../../classes/messages.php");	
+	include_once("../../classes/functions.php");
+	include_once("../../lib/persiandate.php");	
+	$db = Database::GetDatabase();
+	$html = "";
+	$rows = $db->SelectAll("service","*",null,"id ASC");
+	$html.=<<<cd
+	<div class="pages rtl" id="internet">
+	<div class="internet">
+	    <h1>اینترنت</h1>
+	    <div class="buttons">
+		    <span class="horline"></span>
+	    	<span class="verline"></span>
+cd;
+    foreach($rows as $key=>$val) 	
+	{
+		$html.="<a class='mokhaberat active' id='mokhaberat'>{$val[name]}</a>";
+	}
+	$html.=<<<cd
+	<div class="table" id="table"></div>
+		</div>
+	</div>
+</div>
+cd;
+echo $html;
+?>			
+			
+	
